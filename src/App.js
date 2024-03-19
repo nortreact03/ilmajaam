ee4d36257e072f1d5b5dd40d41bff2e9f5474667
Andres Järviste <andres.jarviste@wbscodingschool.com>
2024-03-19 19:58:58 +0200

    loeng6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { loeAndmed } from "./utils";
+import Asukohad from "./Asukohad"
+import Detailid from "./Detailid";
+import { useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const asukohad = [
+    { 
+      nimetus: "Pärnu",
+      lat: 58.3917,
+      long: 24.4953,
+      andmed: null
+    },
+    { 
+      nimetus: "Tallinn",
+      lat: 59.437,
+      long: 24.7536,
+      andmed: null
+    },
+    { 
+      nimetus: "Tartu",
+      lat: 58.3780,
+      long: 26.7290,
+      andmed: null
+    },
+  ];
+
+	const loe = () => {
+		loeAndmed({ lat: 58.3917, long: 24.4953 });
+		loeAndmed({ lat: 59.437, long: 24.7536 });
+	};
+
+  const [aktiivne, setAktiivne] = useState(0)
+
+  const muudaAktiivset = async (index) => {
+    const koht = asukohad[index];
+    setAktiivne(index)
+    const andmed = await loeAndmed({lat: koht.lat, long: koht.long})
+    console.log(andmed)
+  }
+
+	return (
+		<div className="container">
+			<h1>Ilm</h1>
+      <button onClick={loe}>Loe andmed</button>
+			<div className="row">
+        <div className="col-4">
+        <Asukohad asukohad={asukohad}  muudaAktiivset={muudaAktiivset} />
+        </div>
+        <div className="col-8">
+        <Detailid koht={asukohad[aktiivne]}/>
+        </div>
+			</div>
+
+		</div>
+	);
 }
 
 export default App;
