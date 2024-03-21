98ef20f967b280cf06b9ee439b126a7c909f6a0e
Andres Järviste <andres.jarviste@wbscodingschool.com>
2024-03-21 18:55:39 +0200

    loeng7

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { loeAndmed } from "./utils";
 import Asukohad from "./Asukohad"
 import Detailid from "./Detailid";
-import { useState} from 'react'
+import { useState, useEffect} from 'react'
 
 function App() {
 	const asukohad = [
@@ -26,30 +26,31 @@ function App() {
     },
   ];
 
-	const loe = () => {
-		loeAndmed({ lat: 58.3917, long: 24.4953 });
-		loeAndmed({ lat: 59.437, long: 24.7536 });
-	};
-
   const [aktiivne, setAktiivne] = useState(0)
+  const [ilmPraegu, setIlmPraegu] = useState(undefined)
 
   const muudaAktiivset = async (index) => {
     const koht = asukohad[index];
     setAktiivne(index)
     const andmed = await loeAndmed({lat: koht.lat, long: koht.long})
+    setIlmPraegu(andmed)
     console.log(andmed)
   }
+
+  useEffect(() => {
+    console.log('renderdus')
+    muudaAktiivset(1)
+  }, [])
 
 	return (
 		<div className="container">
 			<h1>Ilm</h1>
-      <button onClick={loe}>Loe andmed</button>
-			<div className="row">
+      <div className="row">
         <div className="col-4">
         <Asukohad asukohad={asukohad}  muudaAktiivset={muudaAktiivset} />
         </div>
         <div className="col-8">
-        <Detailid koht={asukohad[aktiivne]}/>
+        <Detailid koht={asukohad[aktiivne]} ilmPraegu={ilmPraegu}/>
         </div>
 			</div>
 
